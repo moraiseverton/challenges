5de0b42d3dd88114cb2575a914fe4a6f330c9e12
Everton Morais <esdmorais@gmail.com>
2020-04-01 23:30:50 -0500

    writers-short-url: Adding gem and generating bitly when creating a writer.

--- app/models/writer.rb
+++ app/models/writer.rb
@@ -1,2 +1,12 @@
 class Writer < ApplicationRecord
+    validates :name, :website, presence: true
+
+    before_create :generate_website_shortened
+
+    def generate_website_shortened
+        client = Bitly::API::Client.new(token: "7000ae3c10755b540f1e1b05329808431ddfa975")
+        bitlink = client.shorten(long_url: self.website)
+        
+        self.website_shortened = bitlink.link
+    end
 end
