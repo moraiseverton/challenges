dc35b915f7ecf6fa3b20370577d74702855f1056
Everton Morais <esdmorais@gmail.com>
2020-04-02 00:59:18 -0500

    writers-short-url: [WIP] Moving shortner URL to controller layer.

--- app/controllers/writers_controller.rb
+++ app/controllers/writers_controller.rb
@@ -25,6 +25,7 @@ class WritersController < ApplicationController
   # POST /writers.json
   def create
     @writer = Writer.new(writer_params)
+    @writer.website_shortened = create_website_shortened
 
     respond_to do |format|
       if @writer.save
@@ -40,7 +41,17 @@ class WritersController < ApplicationController
   # PATCH/PUT /writers/1
   # PATCH/PUT /writers/1.json
   def update
+    changed_website = (@writer.website != writer_params[:website])
+
     respond_to do |format|
+      if (changed_website) 
+        @writer.website_shortened = create_website_shortened
+        params[:website_shortened] = @writer.website_shortened
+
+        puts '@writer.website_shortened', @writer.website_shortened
+        puts 'writer_params', writer_params
+      end
+
       if @writer.update(writer_params)
         format.html { redirect_to @writer, notice: 'Writer was successfully updated.' }
         format.json { render :show, status: :ok, location: @writer }
@@ -70,5 +81,11 @@ class WritersController < ApplicationController
     # Only allow a list of trusted parameters through.
     def writer_params
       params.require(:writer).permit(:name, :website, :website_shortened, :website_headings)
+    end
+
+    def create_website_shortened
+      client = Bitly::API::Client.new(token: "7000ae3c10755b540f1e1b05329808431ddfa975")
+      bitlink = client.shorten(long_url: writer_params[:website])
+      return bitlink.link
     end
 end
