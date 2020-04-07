e1987fa7b88da44ff7634a7f28643c60f01fc331
Everton Morais <esdmorais@gmail.com>
2020-04-06 23:54:07 -0500

    writers-breadcrumb: Adding search field for breadcrumb feature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :writers
+  get "writers/breadcrumb" => "writers#breadcrumb"
+
+  root :to => 'writers#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
