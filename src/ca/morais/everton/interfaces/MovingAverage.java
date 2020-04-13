b77699646c5556dfb32b6ff38d3e85e1226e9c41
Everton Morais <esdmorais@gmail.com>
2020-04-12 23:17:21 -0500

    Adding MovingAverage interface with unit tests

---
+++ src/ca/morais/everton/interfaces/MovingAverage.java
@@ -0,0 +1,93 @@
+package ca.morais.everton.interfaces;
+
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
+public interface MovingAverage<T extends Number> {
+
+    /**
+     *
+     * @return
+     */
+    T initialResult();
+
+    /**
+     * @return
+     */
+    abstract int size();
+
+    /**
+     * @param element
+     */
+    public abstract void add(T element);
+
+    /**
+     *
+     * @param position
+     * @return
+     */
+    default T get(int position) {
+        if (size() <= 0) {
+            throw new NoSuchElementException("No items were added.");
+        }
+        int index = position - 1;
+        if (size() <= index) {
+            throw new NoSuchElementException("Element not found.");
+        }
+        return getElement(index);
+    }
+
+    /**
+     *
+     * @param index
+     * @return
+     */
+    public abstract T getElement(int index);
+
+    /**
+     * @return
+     */
+    public abstract Collection<T> getElements();
+
+    /**
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    abstract T sum(T a, T b);
+
+    /**
+     *
+     * @param a
+     * @param lastElements
+     * @return
+     */
+    abstract T dividedByNumberOfElements(T a, int lastElements);
+
+    /**
+     * @param lastElements
+     * @return
+     */
+    default T average(int lastElements) {
+        if (size() <= 0) {
+            throw new UnsupportedOperationException("There is no item to calculate the average.");
+        }
+
+        if (size() < lastElements) {
+            throw new NoSuchElementException("The number of elements is greater than the number added on the structure. added items: " + (size() + 1) + " - items requrested: " + lastElements);
+        }
+
+        if (size() == 1) {
+            return getElement(0);
+        } else {
+            T accumulator = initialResult();
+
+            for (int index = (size() - lastElements); index < size(); index++) {
+                accumulator = sum(accumulator, getElement(index));
+            }
+
+            return dividedByNumberOfElements(accumulator, lastElements);
+        }
+    }
+}
---
+++ test/ca/morais/everton/interfaces/MovingAverageTest.java
@@ -0,0 +1,173 @@
+package ca.morais.everton.interfaces;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class MovingAverageTest {
+
+    @Test(expected = NoSuchElementException.class)
+    public void get_whenThereIsNoElements_throwsException() {
+        MovingAverage movingAverage = new MovingAverageImplTest();
+
+        int position = new Random().nextInt(Integer.SIZE - 1);
+        movingAverage.get(position);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void get_whenPositionIsGreaterThanSize_throwsException() {
+        MovingAverage movingAverage = new MovingAverageImplTest();
+        movingAverage.add(new Random().nextInt());
+
+        movingAverage.get(movingAverage.size() + 1);
+    }
+
+    @Test
+    public void average_whenPositionExists_returnsItemCorrectly() {
+        MovingAverage movingAverage = new MovingAverageImplTest();
+
+        Random random = new Random();
+        Integer elementA = random.nextInt();
+        Integer elementB = random.nextInt();
+
+        movingAverage.add(elementA);
+        movingAverage.add(elementB);
+
+        Number resultedA = movingAverage.get(1);
+        assertEquals(elementA, resultedA);
+
+        Number resultedB = movingAverage.get(2);
+        assertEquals(elementB, resultedB);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void average_whenThereIsNoElements_throwsException() {
+        MovingAverage movingAverage = new MovingAverageImplTest();
+
+        int lastElements = new Random().nextInt(Integer.SIZE - 1);
+        movingAverage.average(lastElements);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void average_whenNumberOfElementsRequestedIsGreaterThanAdded_throwsException() {
+        MovingAverage movingAverage = new MovingAverageImplTest();
+
+        Random random = new Random();
+        Integer elementA = random.nextInt();
+        Integer elementB = random.nextInt();
+
+        movingAverage.add(elementA);
+        movingAverage.add(elementB);
+
+        final int NUMBER_OF_ELEMENTS = 3;
+        movingAverage.average(NUMBER_OF_ELEMENTS);
+    }
+
+    @Test
+    public void average_whenThereIsOnlyOneElement_returnsItself() {
+        MovingAverage movingAverage = new MovingAverageImplTest();
+
+        Integer element = new Random().nextInt();
+        movingAverage.add(element);
+
+        Number resulted = movingAverage.average(1);
+
+        assertEquals(element, resulted);
+    }
+
+    @Test
+    public void average_whenNumberOfElementsAddedAndRequestedAreTheSame_returnsCalculatedAverage() {
+        MovingAverage movingAverage = new MovingAverageImplTest();
+
+        Random random = new Random();
+        Integer elementA = random.nextInt();
+        Integer elementB = random.nextInt();
+        Integer elementC = random.nextInt();
+
+        movingAverage.add(elementA);
+        movingAverage.add(elementB);
+        movingAverage.add(elementC);
+
+        final int NUMBER_OF_ELEMENTS = 3;
+        Number resulted = movingAverage.average(NUMBER_OF_ELEMENTS);
+
+        Number expected = (elementA + elementB + elementC) / NUMBER_OF_ELEMENTS;
+
+        assertEquals(expected, resulted);
+    }
+
+    @Test
+    public void average_whenNumberOfElementsAddedIsGreaterThanRequestedForAverage_returnsCalculatedAverage() {
+        MovingAverage movingAverage = new MovingAverageImplTest();
+
+        Random random = new Random();
+        Integer elementA = random.nextInt();
+        Integer elementB = random.nextInt();
+        Integer elementC = random.nextInt();
+        Integer elementD = random.nextInt();
+
+        movingAverage.add(elementA);
+        movingAverage.add(elementB);
+        movingAverage.add(elementC);
+        movingAverage.add(elementD);
+
+        final int NUMBER_OF_ELEMENTS = 3;
+        Number resulted = movingAverage.average(NUMBER_OF_ELEMENTS);
+
+        Number expected = (elementB + elementC + elementD) / NUMBER_OF_ELEMENTS;
+
+        assertEquals(expected, resulted);
+    }
+
+    private class MovingAverageImplTest implements MovingAverage<Integer> {
+        private List<Integer> items;
+        private int size;
+
+        MovingAverageImplTest() {
+            this.items = new ArrayList<>();
+            this.size = 0;
+        }
+
+        @Override
+        public Integer initialResult() {
+            return 0;
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+
+        @Override
+        public void add(Integer element) {
+            items.add(element);
+            size++;
+        }
+
+        @Override
+        public Integer getElement(int index) {
+            return items.get(index);
+        }
+
+        @Override
+        public Collection<Integer> getElements() {
+            return items;
+        }
+
+        @Override
+        public Integer sum(Integer a, Integer b) {
+            return a + b;
+        }
+
+        @Override
+        public Integer dividedByNumberOfElements(Integer a, int numberOfElements) {
+            return a / numberOfElements;
+        }
+    }
+}
